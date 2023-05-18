fc035333f770d3b30c412e2f136b4096d1a8386a
Azhar-A-Shaikh <117568791+Azhar-A-Shaikh@users.noreply.github.com>
2023-05-18 11:06:58 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,39 @@
+# Python assignment 1 
+
+# print (Hello world)
+print("Hello World")
+
+# Program to add and divide 
+n1 = 8
+n2 = 2
+
+add = 8+2
+div = 8/2
+print(add)
+print(div)
+
+# Program to find area of triangle
+height = 3
+base = 4
+
+area = (height*base)/ 2
+print(area)
+
+# Program to swap 2 numbers 
+
+num1 = 2
+num2 = 3
+
+print(num1)
+print(num2)
+
+num1,num2 = num2,num1
+
+print(num1)
+print(num2)
+
+# program to generate a random number 
+import random
+
+print(random.random())
+print(random.randint(1,100))
